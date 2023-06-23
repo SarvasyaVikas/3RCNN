4d73e7ac029fe1567dcf23998f3d97cf196a6d5d
Vikas Sarvasya <85037039+SarvasyaVikas@users.noreply.github.com>
2023-06-23 11:18:09 -0400

    Add files via upload

---
+++ 80Processors/sets.py
@@ -0,0 +1,22 @@
+import random
+
+class SETS:
+	def __init__(self):
+		pass
+	
+	def data(prob):
+		mult = [[1, 0, 40], [3, 40, 60], [4, 20, 40], [5, 40, 60], [6, 40, 60], [10, 0, 40], [12, 50, 70], [13, 50, 70], [14, 50, 70], [15, 30, 50], [17, 25, 45], [18, 40, 60], [19, 70, 90], [22, 60, 80], [23, 0, 60], [24, 40, 60], [26, 45, 65], [27, 20, 60], [28, 50, 70]]
+		
+		fin = []
+		for i in range(len(mult)):
+			if random.random() <= prob:
+				fin.append(mult[i])
+		
+		return fin
+	
+	def imgs(mult):
+		tot = 0
+		for i in range(len(mult)):
+			diff = mult[i][2] - mult[i][1]
+			tot += diff
+		print(tot)
